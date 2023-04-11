10012862283f48ce1b241648cfbe549f4149b6c7
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-04-10 22:28:22 -0300

    Project created

---
+++ src/main/java/com/example/finanx/FinanXApplication.java
@@ -0,0 +1,13 @@
+package com.example.finanx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finanx/FinanXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finanx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
